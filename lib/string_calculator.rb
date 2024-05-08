6a603ffc777eff3d4ad195fe7a49c0e2b4c38e33
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2024-05-09 05:20:09 +0530

    code to handle negative number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,11 +4,19 @@ class StringCalculator
             0
         elsif /,\n|,\z|\n,|\n\z/.match?(string)
             raise ArgumentError, "Invalid input"
-        elsif string.start_with?("//")
-            delimiter, numbers = string[2..-1].split("\n")
-            numbers.split(delimiter).map(&:to_i).sum
         else
-            string.gsub('\n', ',').split(',').map(&:to_i).sum
+            if string.start_with?("//")
+                delimiter, numbers = string[2..-1].split("\n")
+                numbers = numbers.split(delimiter).map(&:to_i)
+            else
+                numbers = string.gsub('\n', ',').split(',').map(&:to_i)
+            end
+            negative_numbers = numbers.select { |num| num < 0 }
+            if negative_numbers.any?
+                raise RuntimeError, "negative numbers not allowed: #{negative_numbers.join(", ")}"
+            else
+                numbers.sum
+            end
         end
     end
 end
