a66b8b99d4f4545924e237a565b0955946117460
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2024-05-09 05:02:06 +0530

    code to handle invalid input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,15 @@ class StringCalculator
     def add(string)
         if string.empty?
             0
+        elsif /,\n|,\z|\n,|\n\z/.match?(string)
+            raise ArgumentError, "Invalid input"
         else
             string.gsub('\n', ',').split(',').map(&:to_i).sum
         end
     end
 end
+
+
+
+
+
